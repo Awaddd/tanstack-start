c8d2c1fb64ff58e04c4fbd8401bbd850526f365c
Awaddd <34748292+Awaddd@users.noreply.github.com>
2025-06-10 17:10:09 +0100

    Simplify example

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,48 +1,31 @@
-// app/routes/index.tsx
-import * as fs from 'node:fs'
-import { createFileRoute, useRouter } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start'
+import { useState } from 'react'
 
-const filePath = 'count.txt'
+const greetTom = createServerFn({ method: "GET" }).handler(() => "Hello Tom")
 
-async function readCount() {
-    return parseInt(
-        await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-    )
-}
-
-const getCount = createServerFn({
-    method: 'GET',
-}).handler(() => {
-    return readCount()
-})
-
-const updateCount = createServerFn({ method: 'POST' })
-    .validator((d: number) => d)
-    .handler(async ({ data }) => {
-        const count = await readCount()
-        await fs.promises.writeFile(filePath, `${count + data}`)
-    })
+const greetSomeone = createServerFn({ method: "POST" })
+    .validator((name: string) => name)
+    .handler(({ data }) => "Hello " + data)
 
 export const Route = createFileRoute('/')({
     component: Home,
-    loader: async () => await getCount(),
+    loader: async () => await greetTom(),
 })
 
 function Home() {
-    const router = useRouter()
-    const state = Route.useLoaderData()
+    const [greeting, setGreeting] = useState<string>(Route.useLoaderData())
 
     return (
         <button
             type="button"
             onClick={() => {
-                updateCount({ data: 1 }).then(() => {
-                    router.invalidate()
+                greetSomeone({ data: "Jane" }).then((data) => {
+                    setGreeting(data)
                 })
             }}
         >
-            Add 1 to {state}?
+            {greeting}
         </button>
     )
 }
